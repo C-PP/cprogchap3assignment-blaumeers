f9342a3856b6c85231d61dc1732f8e8f243f4699
blaumeers <43158581+blaumeers@users.noreply.github.com>
2018-09-30 21:22:32 +0900

    201525140 김성락
    Assignment3

--- CProgChap3Assignment22/CProgChap3Assignment2/platinum.cpp
+++ CProgChap3Assignment22/CProgChap3Assignment2/platinum.cpp
@@ -1,34 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-void function(void);
-
+void function(float weight);
 /* ANSI/ISO C function prototyping */
-int main(void) 
-{
+int main(void) {
 	float weight;    /* user weight             */
 
-	printf("Are you worth your weight in platinum?\n");
-	printf("Let's check it out.\n");
-	printf("Please enter your weight in pounds: ");/*get input from the user  */
+	printf("Enter a floating-point value:");
 	scanf("%f", &weight);      /* assume platinum is $1700 per ounce          */
-							   /* 14.5833 converts pounds  */
-	printf("I will summon the butler function.\n");
+							   /* 14.5833 converts pounds	*/
 	function(weight);
-	printf("Yes. Bring me some tea and writeable DVDs.\n");
 	getchar();
 	return 0;
 }
 
 void function(float weight)       /* start of function definition */
 	{
+
 		float value;     /* platinum equivalent     */
 
-		value = 1700.0 * weight * 14.5833;
-		printf("weight = %f\n", weight);
-		//여기에 코딩하는 것이 필요함
-		printf("Your weight in platinum is worth $%.2f.\n", value); // 소숫점 두자리 출력
-		printf("You are easily worth that! If platinum prices drop,\n");
-		printf("eat more to maintain your value.\n");
+		printf("fixed-point notation: %f\n", weight);
+		printf("exponential notation: %e\n", weight);
+		printf("p notation: %a,\n", weight);
 		system("pause");
-		return 0;
+		return;
 	}
